8beb95db200952c02b8a7191a0693d165bf3f4a8
fnfpwp <117086223+pwp114514@users.noreply.github.com>
2023-09-01 23:53:55 +0800

    Update ClientPrefs.hx

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -4,6 +4,7 @@ import flixel.FlxG;
 import flixel.util.FlxSave;
 import flixel.input.keyboard.FlxKey;
 import flixel.graphics.FlxGraphic;
+import gamejolt.GameJoltAPI;
 import Controls;
 
 class ClientPrefs {
