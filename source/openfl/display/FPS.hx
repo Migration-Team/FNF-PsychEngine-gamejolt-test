3acf70d9f0778c2193a355557afcafb5e0e03aa1
fnfpwp <117086223+pwp114514@users.noreply.github.com>
2023-09-02 14:50:58 +0800

    Update FPS.hx

--- source/openfl/display/FPS.hx
+++ source/openfl/display/FPS.hx
@@ -95,6 +95,8 @@ class FPS extends TextField
 			memoryMegas = Math.abs(FlxMath.roundDecimal(System.totalMemory / 1000000, 1));
 			text += "\nMemory: " + memoryMegas + " MB";
 			#end
+			peV = MainMenuState.psychEngineVersion;
+			text += "\nPsychEngineVersion V " + peV;
 			
 			textColor = 0xFFFFFFFF;
 			if (memoryMegas > 3000 || currentFPS <= ClientPrefs.framerate / 2)
